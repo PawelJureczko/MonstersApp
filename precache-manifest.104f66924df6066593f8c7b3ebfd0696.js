49c2e0f0eaa9ee474ddb95804c8785a5819effe0
Paweu <paloboo@o2.pl>
2020-05-25 19:03:29 +0200

    Updates

---
+++ precache-manifest.104f66924df6066593f8c7b3ebfd0696.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f6c5b494cd1c98a5f2bd475a92e1225",
+    "url": "/MonstersApp/index.html"
+  },
+  {
+    "revision": "8981f646e045760294d5",
+    "url": "/MonstersApp/static/css/main.bee4390d.chunk.css"
+  },
+  {
+    "revision": "3045bc828fe84460bc3c",
+    "url": "/MonstersApp/static/js/2.e667cf7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersApp/static/js/2.e667cf7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8981f646e045760294d5",
+    "url": "/MonstersApp/static/js/main.147aa78f.chunk.js"
+  },
+  {
+    "revision": "bb4e370999640bc042c5",
+    "url": "/MonstersApp/static/js/runtime-main.88bcecbf.js"
+  }
+]);
